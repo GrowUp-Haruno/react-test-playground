0da9b8df945ec8124c0a7ff4f6e82fb85add0fc4
Daisuke Wada <74647388+GrowUp-Haruno@users.noreply.github.com>
2022-03-31 17:10:47 +0900

    - ボタンをスタイリング - ボタンを無効化するチェックボックスを追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,42 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { App } from './App';
 
 describe('App', () => {
-  it('button has correct initial text', () => {
+  it('button has correct initial text and color', () => {
     render(<App />);
     const button = screen.getByRole('button', { name: 'Change to blue' });
+
     expect(button).toBeInTheDocument();
+    expect(button).toHaveStyle({ backgroundColor: 'red' });
+
+    userEvent.click(button);
+
+    expect(button).toHaveStyle({ backgroundColor: 'blue' });
+    expect(button).toHaveTextContent('Change to red');
   });
-  it('button has correct initial color', () => {
+
+  it('initial conditions', () => {
     render(<App />);
     const button = screen.getByRole('button', { name: 'Change to blue' });
-    expect(button).toHaveStyle({ backgroundColor: 'red' });
+    const checkbox = screen.getByRole('checkbox');
+
+    // check that the button starts out enable
+    expect(button).toBeEnabled();
+
+    // check that the checkbox starts out unchecked
+    expect(checkbox).not.toBeChecked();
+  });
+
+  it('Checkbox disables button on first click and enables on second click', () => {
+    render(<App />);
+    const button = screen.getByRole('button',{name: 'Change to blue'});
+    const checkbox = screen.getByRole('checkbox', {name:'Disable button'});
+
+    userEvent.click(checkbox);
+    expect(button).toBeDisabled();
+
+    userEvent.click(checkbox);
+    expect(button).toBeEnabled();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import { useMemo, useState } from 'react';
+
 export function App() {
+  const [buttonColor, setButtonColor] = useState<'red' | 'blue'>('red');
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const newButtonColor = useMemo<'red' | 'blue'>(() => {
+    return buttonColor === 'red' ? 'blue' : 'red';
+  }, [buttonColor]);
+
   return (
     <div>
-      <button style={{ backgroundColor: 'red' }}>Change to blue</button>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={isDisabled}
+      >
+        Change to {newButtonColor}
+      </button>
+
+      <br />
+
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={isDisabled}
+        aria-checked={isDisabled}
+        onChange={(e) => {
+          setIsDisabled(e.target.checked);
+        }}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
